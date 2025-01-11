02602db98f675b4fbf14d0a8d7c80686502d50cf
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-01-11 13:29:13 -0300

    feat(routes/cadastro): create new route named cadastro

--- routes.js
+++ routes.js
@@ -5,7 +5,11 @@ const { marked } = require("marked");
 const path = require("path");
 const IpGeolotation = require("./infra/ip.js");
 const User = require("./models/Users.js");
+const Date = require("./infra/Date.js");
+const formatUser = require("./user/formatUser.js");
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.engine("hbs", hbs.engine({
   extname: ".hbs",
@@ -25,8 +29,8 @@ app.get("/", async(req, res)=>{
 
   if(user === null){
     const buttons = `
-    <button type='button' class='btn btn-sm btn-outline-light' onclick="location.href='/login'">Entrar</button>
-    <button type='button' class='btn btn-sm btn-outline-warning' onclick="location.href='/register'">Cadastrar</button>
+    <button type='button' class='btn btn-sm bt-outline-dark' onclick="location.href='/login'">Entrar</button>
+    <button type='button' class='btn btn-sm btn-dark' onclick="location.href='/cadastro'">Cadastrar</button>
     `
     res.render("home", {
       buttons
@@ -34,10 +38,65 @@ app.get("/", async(req, res)=>{
   }
   else{
     const buttons = `
-    <a href="/@${user["nome"]}" class="text-white"><strong>@${user["nome"]}</strong></a>
+    <a href="/@${user["nome"]}" class="text-muted">${user["nome"]}</a>
     `
     res.render("home", {
       buttons
     })
   }
+})
+app.get('/cadastro', async(req, res)=>{
+  const ip = await IpGeolotation()
+
+  const user = await User.findOne({
+    where: {
+      ip: ip.ip
+    }
+  })
+  
+  if(user === null){
+    const buttons = `
+    <button type='button' class='btn btn-sm bt-outline-dark' onclick="location.href='/login'">Entrar</button>
+    <button type='button' class='btn btn-sm btn-dark' onclick="location.href='/cadastro'">Cadastrar</button>
+    `
+    res.render("cadastro", {
+      buttons
+    })
+  }else{
+    res.redirect('/')
+  }
+})
+app.post('/cadastro', async(req, res)=>{
+  const ip = await IpGeolotation()
+  const date = Date()
+  const { email, senha } = req.body
+  const biografia = marked("##### Olá mundo")
+  const nome = formatUser(req.body.nome)
+
+  const findUser = await User.findOne({
+    where: {
+      nome,
+      email
+    }
+  })
+  if(findUser === null){
+    await User.create({
+      nome,
+      email,
+      senha,
+      biografia,
+      ip: ip.ip,
+      data: date
+    })
+    res.redirect('/')
+  }else{
+    res.render("cadastro", {
+      message: `
+      <div class="alert alert-danger" role="alert">
+        Já existe um usuário com este nome ou email.
+      </div>      
+      `
+    })
+  }
+  
 })
