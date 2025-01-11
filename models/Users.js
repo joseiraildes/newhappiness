6ef5bc2810602a30b59f880a6b081f2457d4b10f
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-01-11 11:02:49 -0300

    feat(models/User): create a new model

---
+++ models/Users.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/databse.js");
+
+const User = sequelize.define("users", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [8, 160]
+    }
+  },
+  biografia: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  ip: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  data: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+})
+
+module.exports = User;
