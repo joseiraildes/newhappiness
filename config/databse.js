e2cbfeee4313fa8b0a1bfc50a1d3a3ce63553dde
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-01-11 21:36:59 -0300

    feat(errorrrr)

--- config/databse.js
+++ config/databse.js
@@ -1,12 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("mysql://root:wLwvrMApzIesReRtFMtlhSZcSxSkikju@autorack.proxy.rlwy.net:11626/railway", {
-  host: "autorack.proxy.rlwy.net",
-  port: 11626,
-  dialect: "mysql",
-  username: "root",
-  password: "wLwvrMApzIesReRtFMtlhSZcSxSkikju",
-  database: "railway"
-})
+const sequelize = new Sequelize("mysql://root:wLwvrMApzIesReRtFMtlhSZcSxSkikju@autorack.proxy.rlwy.net:11626/railway")
 
 module.exports = sequelize;
